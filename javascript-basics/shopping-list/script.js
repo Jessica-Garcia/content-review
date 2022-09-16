033ba29b76326a67e4f41c68e83f5985f8c8b829
Jéssica Garcia <jessica.rodriguesgarcia@gmail.com>
2022-09-16 15:36:57 -0300

    Implemented item update function

--- javascript-basics/shopping-list/script.js
+++ javascript-basics/shopping-list/script.js
@@ -2,7 +2,7 @@ const itemInput = document.getElementById('item-name-input');
 const addItemButton = document.getElementById('add-item-button');
 const list = document.getElementById('item-list-container');
 const shoppingList = localStorage.getItem('shopping-list');
-const dbItems =  shoppingList
+let dbItems =  shoppingList
     ? JSON.parse(shoppingList)
     : [];
 
@@ -11,8 +11,16 @@ const modal= document.getElementById('modal');
 const fade= document.getElementById('fade');
 const updateButton = document.getElementById('update-item-button');
 const itemUpdateInput = document.getElementById('item-update-input');
+let selectedId;
 
-const toggleModal = () => {
+const toggleModal = (id) => {
+    if(id) {
+        itemUpdateInput.value = dbItems.find(item => item.id === id).name;
+        selectedId = id;
+    } else{
+        itemUpdateInput.value = '';
+        selectedId = null;
+    }
     modal.classList.toggle('hide');
     fade.classList.toggle('hide');
 }
@@ -23,7 +31,10 @@ const saveItemInList = () => {
     
     const inputValue = itemInput.value;
     itemInput.value = '';
-    if(!inputValue.trim().length) return;
+    if(!inputValue.trim().length) {
+        alert('Adicione um texto');
+        return;
+    }
 
     const newItem = {
         name: inputValue,
@@ -34,7 +45,6 @@ const saveItemInList = () => {
     localStorage.setItem('shopping-list', JSON.stringify(dbItems));
     showList();
 }
-
 
 const showList = () => {
     list.innerText = '';
@@ -49,9 +59,10 @@ const showList = () => {
 
         const checkInput = document.createElement('input');
         checkInput.setAttribute('type', 'checkbox');
+        checkInput.checked = item.checked;
         checkInput.setAttribute('id', item.id);
         checkInput.classList.add('checkbox-input');
-
+        checkInput.addEventListener('click', () => toggleCheckbox(item.id))
         const label = document.createElement('label');
         label.classList.add('item-text');
         label.setAttribute('for', item.id);
@@ -67,9 +78,9 @@ const showList = () => {
         const editButton = document.createElement('button');
         editButton.classList.add('action-button');
         editButton.innerHTML = '<i class="fa-solid fa-pencil">';
-        editButton.addEventListener('click', () => toggleModal());
-        updateButton.addEventListener('click', toggleModal);
-
+        editButton.addEventListener('click', ()=> toggleModal(item.id));
+        updateButton.addEventListener('click', updateItem);
+        
         const deleteButton = document.createElement('button');
         deleteButton.classList.add('action-button');
         deleteButton.innerHTML = '<i class="fa-solid fa-trash">';
@@ -84,10 +95,40 @@ const showList = () => {
 
 const deleteItemById = (id) => {
     const index = dbItems.findIndex(item => item.id === id);
-    console.log(index);
     dbItems.splice(index, 1);
     localStorage.setItem('shopping-list', JSON.stringify(dbItems));
     showList();
+}
+
+const updateItem = () => {
+    if(!itemUpdateInput.value.trim().length) {
+        alert('Adicione um texto');
+        return;
+    }
+    dbItems = dbItems.map(item => {
+        if(item.id === selectedId){
+            return {
+                ...item, 
+                name: itemUpdateInput.value
+            }
+        }
+        return item;
+    })
+    localStorage.setItem('shopping-list', JSON.stringify(dbItems));
+    toggleModal();
+    showList();
+}
+
+const toggleCheckbox = (id) => {
+    dbItems = dbItems.map(item => {
+        return item.id === id 
+            ? {
+                ...item, 
+                checked: !item.checked
+            } 
+            : item;
+    })
+    localStorage.setItem('shopping-list', JSON.stringify(dbItems));
 }
 
 const saveItemWithEnterKey = (e) => {
@@ -98,7 +139,7 @@ const saveItemWithEnterKey = (e) => {
 }
 
 addItemButton.addEventListener('click', saveItemInList);
-document.addEventListener('keypress', saveItemWithEnterKey);
+itemInput.addEventListener('keypress', saveItemWithEnterKey);
+itemUpdateInput.addEventListener("keypress", (e) => e.key === 'Enter' && updateItem());
 closeModalButton.addEventListener('click', () => toggleModal());
-
 showList();
